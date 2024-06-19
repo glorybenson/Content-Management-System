30d3e318583a2c9cb88441eeedbd3b1cb4bbec67
glorybenson <glorybson3738@gmail.com>
2024-06-19 19:29:35 +0100

    project update

---
+++ Public/staff/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>GBI</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+        <h1>GBI Staff area</h1>
+    </header>
+    <navigation>
+        <ul>
+            <li><a href="index.php">Menu</a></li>
+        </ul>
+    </navigation>
+    <footer>
+        &COPY; <?php echo date ('Y')?> Global Bank
+    </footer>
+</body>
+</html>
